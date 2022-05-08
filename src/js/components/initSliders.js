11247c018189cdc5c720d9a903f45f004b5d677f
Maksym <maksymRozhko7@gmail.com>
2022-05-08 22:13:19 +0300

    feat: new section our projects, init slider, adaptive

--- src/js/components/initSliders.js
+++ src/js/components/initSliders.js
@@ -10,3 +10,28 @@ new Swiper('.swiper.main-slider', {
         prevEl: '.main-slider .swiper-button-prev',
     },
 });
+
+new Swiper('.swiper.projects-slider', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        575: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+        1440: {
+          slidesPerView: 4,
+        }
+    },
+});
