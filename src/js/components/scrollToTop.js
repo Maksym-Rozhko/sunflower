2c0b593a15c45c27959283c7812cb1ff5022d7c6
Maksym <maksymRozhko7@gmail.com>
2022-05-10 00:59:15 +0300

    feat: 404 page, adaptive. fix: one news page, scroll to top

--- src/js/components/scrollToTop.js
+++ src/js/components/scrollToTop.js
@@ -1,12 +1,14 @@
 const btnScrollUp = document.querySelector('.scroll-top');
 const topElem = document.querySelector('.main'); 
 
-btnScrollUp.addEventListener('click', () => {
-  topElem.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start'
+try {
+  btnScrollUp.addEventListener('click', () => {
+    topElem.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  //   jQuery("html, body").animate({
+  //     scrollTop: "0"
+  // }, 700);
   });
-//   jQuery("html, body").animate({
-//     scrollTop: "0"
-// }, 700);
-});
+} catch (error) {}
